2a0e62ea2c7409f41d2def31189a94ac0024022c
Eric Andrechek <eric@andrechek.com>
2020-11-17 00:23:16 -0500

    troubleshooting motion issues

--- motion/boot.sh
+++ motion/boot.sh
@@ -3,4 +3,4 @@
 sleep 10 &
 su - pi -c "screen -S main -dm bash -c 'cd /home/pi/TrailCam/motion && python3 filehandler.py'" &
 date > /home/pi/TrailCam/motion/startlog.txt
-su - pi -c "screen -S main -dm bash -c 'sudo motion -c /home/pi/TrailCam/motion/motion conf'" &
+screen -S main -dm bash -c 'sudo motion -c /home/pi/TrailCam/motion/motion conf' &
